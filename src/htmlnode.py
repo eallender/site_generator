4f235332d4098c525cfd97ba07d109ce4d4989be
Evan Allender <evan.allender@gmail.com>
2025-07-24 22:13:26 -0500

    fix: bug with LeafNode value checking

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -35,10 +35,10 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
+        if self.value is None:
             return ValueError("LeafNode with no value")
 
-        if not self.tag:
+        if self.tag is None:
             return self.value
 
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
